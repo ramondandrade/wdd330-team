00508fc62a29b99511cdfa3bd9e24620d05cbf6e
Ramon Andrade <ramondandrade@yahoo.com>
2025-07-11 00:00:35 +0200

    Week 2 - changes

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -3,7 +3,7 @@ import { renderListWithTemplate } from "./utils.mjs";
 function productCardTemplate(product) {
 
     return `<li class="product-card">
-      <a href="product_pages/index.html?products=${product.Id}">
+      <a href="product_pages/?product=${product.Id}">
         <img src="${product.Image}" alt="Image of ${product.Name}">
         <h2 class="card__brand">${product.Brand.Name}</h2>
         <h3 class="card__name">${product.Name}</h3>
