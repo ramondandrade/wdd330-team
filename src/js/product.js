e820c6820c481ea8c263c686add0fce6766388f6
Ramon Andrade <ramondandrade@yahoo.com>
2025-07-10 23:09:12 +0200

    week 2 - team (lint)

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,8 @@
-import { setLocalStorage, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-const productId = getParam('product'); // Product ID
+const productId = getParam("product"); // Product ID
 const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productId, dataSource);
