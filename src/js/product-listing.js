24269689f89155ca189418b88066614e0c443f36
Ramon Andrade <ramondandrade@yahoo.com>
2025-07-26 11:29:59 +0200

    Week 4 - team

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,11 +1,11 @@
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter, getParam } from "./utils.mjs";
 
 loadHeaderFooter();
 
 const categoryId = getParam("category");
-const dataSource = new ProductData(categoryId);
+const dataSource = new ExternalServices();
 const element = document.querySelector(".product-list");
 const productList = new ProductList(categoryId, dataSource, element);
 
--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,11 @@
 import { getParam } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 const productId = getParam("product"); // Product ID
-const dataSource = new ProductData();
+const dataSource = new ExternalServices();
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
